0b481b4286d35446087477488f3bc46e48d84c0c
Galt009 <51437263+Galt009@users.noreply.github.com>
2019-08-21 22:07:47 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 //由于 webpack 是基于Node进行构建的，所以webpack的配置文件中，任何合法的Node代码都是支持的
 var path =  require('path')
 var htmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 //当以命令行形式运行 webpack 或 webpack-dev-server 的时候，工具会发现，我们并没有提供要打包的文件的 入口 和 出口 文件，此时，工具会检查根目录中的配置文件，并读取这个文件，就拿到了导出的这个对象，进行打包构建
 module.exports = {
@@ -13,7 +14,8 @@ module.exports = {
         new htmlWebpackPlugin({
         template: path.join(__dirname, './src/index.html'), //指定模板文件路径
         filename: 'index.html' //设置生成的内存页面的名称
-        })
+        }),
+        new VueLoaderPlugin()
     ],
     module: {     //配置所有第三方loader模块
         rules: [     //第三方模块的匹配规则
@@ -23,7 +25,8 @@ module.exports = {
             { test: /\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader?limit=6000&name=[hash:8]-[name].[ext]' },
             //处理字体文件
             { test:/\.(ttf|eot|svg|woff|woff2|otf)$/, use: 'url-loader'},
-            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.vue$/, use: 'vue-loader' } // 处理 .vue 文件的 loader
         ]
     }
 }
